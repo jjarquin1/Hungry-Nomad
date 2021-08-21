f359eb1056368c9dbfc63e41fb50a733df7a566b
Minh Nguyen <mnguyen728@hotmail.com>
2021-08-21 10:53:40 -0700

    joe

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import {useState} from "react";
 import P1 from './pages/PageOne'
 import P2 from './pages/PageTwo'
 import P3 from './pages/PageThree'
+import Navbar from './components/navbar';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+
+
 
 function App() {
   const [page, setPage] = useState("one");
@@ -21,11 +26,19 @@ function App() {
 
   return (
     <div className="App">
+      <Router>
+
+      <div>
+        <Navbar>
+
+        </Navbar>
+      </div>
       <Button onClick={() => setPage("one")}/>
       <Button onClick={() => setPage("two")}/>
       <Button onClick={() => setPage("three")}/>
       <button onClick={()=> setCount(count+1)}>{count}</button>
       {renderPage()}
+      </Router>
     </div>
   );
 }
