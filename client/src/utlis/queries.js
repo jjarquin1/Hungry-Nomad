fea6911a045db0267ffda6222d38f3861db7a897
Drigo <rodrigoaguayo94@gmail.com>
2021-08-21 22:13:24 -0700

    added mutations to keep user input, debugged some issues, added queries

--- client/src/utlis/queries.js
+++ client/src/utlis/queries.js
@@ -1,16 +1,21 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USER = gql`
+export const QUERY_SINGLE_USER = gql`
   query user($username: String!) {
     user(username: $username) {
       _id
       username
       email
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-      }
+    }
+  }
+`;
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      skills
     }
   }
 `;
