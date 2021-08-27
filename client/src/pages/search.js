47c9e719be799f99f670f4417ef84397d6718aa3
Drigo <rodrigoaguayo94@gmail.com>
2021-08-26 19:42:24 -0700

    styling

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -19,24 +19,36 @@ const Search = () => {
     return (
         <div>
             {
-                businesses.map(biz => (
-                    <div className='card text-center'>
-                        <h3>{biz.name}</h3>
-                        <img
-                            //src='https://via.placeholder.com/150'
-                            src={biz.image_url}
-                            alt=''
-                            className='round-img'
-                            style={{ width: '210px', height: '210px', boarderRadis: '200px' }}
-                        />
-                        <div></div>
-                        <p>info here</p>
-                        <p>{biz.location.address1}</p>
-                        {/* <p><BusinessRating /></p> */}
+                businesses.map(trucks => (
+                    <div className="container card">
+                        <div class="row align-items-start">
+
+                            <div className='col'>
+                                <h3>{trucks.name}</h3>
+                                <img
+                                    //src='https://via.placeholder.com/150'
+                                    src={trucks.image_url}
+                                    alt='https://via.placeholder.com/150'
+                                    className='round-img'
+                                    style={{ width: '210px', height: '210px', boarderRadis: '200px' }}
+                                />
+                            </div>
+                            <div className='col'>
+                                <p>{trucks.rating}</p>
+                                <p>{trucks.location.address1}</p>
+                                <p>{trucks.location.city}</p>
+                                <p>{trucks.location.state}</p>
+                                <button className='button'> URL </button>
+                            </div>
+
+                        </div>
                     </div>
+
+
+
                 ))
             }
-        </div>
+        </div >
     );
 }
 
