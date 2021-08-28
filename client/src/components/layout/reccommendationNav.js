8eeb1381c128621d4b56ae46776409c1452b4846
Minh Nguyen <mnguyen728@hotmail.com>
2021-08-28 00:55:13 -0700

    css

--- client/src/components/layout/reccommendationNav.js
+++ client/src/components/layout/reccommendationNav.js
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom';
 
 
-const RecNav = () => {
-    return (
-        <main>
-            <navbar class="nav d-flex justify-content-evenly">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link" href="#">Link</a>
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </navbar>
-        </main>
+
+const RecNav = () =>
+{
+    return(
+    <main>
+        <nav className="nav d-flex justify-content-evenly">
+            <a className="nav-link active" aria-current="page" href="#">Mexican</a>
+            <a className="nav-link" href="#">Fusion</a>
+            <a className="nav-link" href="#">Asian</a>
+            <a className="nav-link" href="#">BBQ</a>
+            <a className="nav-link" href="#">Hot Dogs</a>
+        </nav>
+    </main>
     )
 }
 
