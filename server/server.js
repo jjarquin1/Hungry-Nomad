e03b8575bbaaf530fc795caac9684353ef3f95cb
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-08-28 09:37:09 -0700

    ok

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/public')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
