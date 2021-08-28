d29bf99fcab30bf2e3d0e27a1e0127926e9ef1aa
Minh Nguyen <mnguyen728@hotmail.com>
2021-08-27 21:15:57 -0700

    navbar issue

--- client/src/components/layout/reccommendationNav.js
+++ client/src/components/layout/reccommendationNav.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const RecNav = () =>{
     return(
-        <nav class="nav d-flex justify-content-evenly">
+        <navbar class="nav d-flex justify-content-evenly">
             <a class="nav-link active" aria-current="page" href="#">Active</a>
             <a class="nav-link" href="#">Link</a>
             <a class="nav-link" href="#">Link</a>
             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </nav>
+        </navbar>
     )
 }
 
